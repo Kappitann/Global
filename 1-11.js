b8092e43621586b04d913c68ebcab4d8d5967996
Sviatoslav Stepanenko <svatoslavstepanenko224@gmail.com>
2021-03-02 22:29:46 +0200

    Tast 2

--- 1-11.js
+++ 1-11.js
@@ -6,4 +6,12 @@
     }
     console.log(seconds(100));
 }
+//2
+{
+    console.log('task 2:');
+    function perimeter(side, count){
+        return side*count;
+    }
+    console.log(perimeter(3,5));
+}
 
