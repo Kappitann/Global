bccefabbf6233e24e629c04d8460e24c73c0b39e
Sviatoslav Stepanenko <svatoslavstepanenko224@gmail.com>
2021-03-02 22:32:32 +0200

    Task 3

--- 1-11.js
+++ 1-11.js
@@ -14,4 +14,25 @@
     }
     console.log(perimeter(3,5));
 }
+//3
+{
+    console.log('task 3:');
+    function task3(n){
+        for (let i = 1; i < n; i++) {
+            if(i%5==0&&i%3==0){
+                console.log('fizzbuzz');
+            }
+            else if(i%3==0){
+                console.log(`fizz`);
+            }
+            else if(i%5==0){
+                console.log('buzz');
+            }
+            else{
+                console.log(i);
+            }
+        }
+    }
+    task3(16);
+}
 
