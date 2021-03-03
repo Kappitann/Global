2e4508d7077d8ca391233f8a28f8cf3269c5efdc
Sviatoslav Stepanenko <svatoslavstepanenko224@gmail.com>
2021-03-03 10:25:31 +0200

    Task 9

---
+++ Pz_1.13_9.js
@@ -0,0 +1,19 @@
+
+class Worker {
+    constructor(name, surname, rate, days) {
+        this.name = name;
+        this.surname = surname;
+        this.rate = rate;
+        this.days = days;
+    }
+    getSalary() { return console.log(`Salary worker: ${this.days * this.days}`) }
+}
+let w1 = new Worker('Misha', 'Reshetnikov', 400, 15)
+let w2 = new Worker('Shmidt', 'Kovalenko', 150, 10)
+let w3 = new Worker('Alex', 'Komisarov', 340, 20)
+w1.getSalary();
+w2.getSalary();
+w3.getSalary();
+console.log(w1)
+console.log(w2)
+console.log(w3)
